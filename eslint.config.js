7d2082bc041be3a96c6a003b271b464bacfcc786
espinozagabriel0 <bascopeespinozagabriel@fpllefia.com>
2024-12-03 19:04:44 +0100

    Configurar eslint para solucionar jsx error

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import pluginReact, { rules } from "eslint-plugin-react";
 
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -9,4 +9,10 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off"
+    }
+  }
 ];
