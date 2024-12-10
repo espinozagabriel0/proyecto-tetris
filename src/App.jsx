68f5399810c03e3a165f381e5c89d77ace83e34a
espinozagabriel0 <bascopeespinozagabriel@fpllefia.com>
2024-12-10 20:27:34 +0100

    estados ventana modal, formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
 import TablaPartidas from './components/TablaPartidas'
 import VentanaModal from './components/VentanaModal'
+import { useState } from "react"
 
 function App() {
-  
+
+  const [data, setData] = useState([
+      {id: 1, avatar: "🦊", nick: "FoxPlayer", puntos: 1500, fecha: "2024-11-30"},
+      {id: 2, avatar: "🐼", nick: "Gabriel", puntos: 2200, fecha: "2024-12-01"},
+      {id: 3, avatar: "🦁", nick: "LionKing", puntos: 1800, fecha: "2024-12-02"},
+      {id: 4, avatar: "🐯", nick: "TigerStripe", puntos: 2000, fecha: "2024-12-02"},
+      {id: 5, avatar: "🐸", nick: "FrogJumper", puntos: 1700, fecha: "2024-12-03"},
+      {id: 6, avatar: "prueba1", nick: "ampersand", puntos: 9000, fecha: "2021-12-03"},
+      {id: 7, avatar: "prueba2", nick: "bala", puntos: 4500, fecha: "2024-12-10"}
+    ]
+  )
   return (
     <div className="">
-      <TablaPartidas/>
+      <TablaPartidas
+        data={data}
+        setData={setData}
+      />
+
       {/* TODO: Añade botón y modal para agregar nuevas partidas */}
       {/* <button className='rounded'>Añadir Partida</button> */}
        
-    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-      Añadir Partida
-    </button>
-    <VentanaModal/>
+      <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Añadir Partida
+      </button>
+      <VentanaModal
+        data={data}
+        setData={setData}
+      />
     </div>
   )
 }
--- src/components/VentanaModal.jsx
+++ src/components/VentanaModal.jsx
@@ -1,5 +1,22 @@
-export default function VentanaModal() {
-  return (
+import { useState } from "react"
+
+export default function VentanaModal({data, setData}) {
+
+    const [avatar, setAvatar] = useState('')
+    const [nick, setNick] = useState('')
+    const [puntuacion, setPuntuacion] = useState('')
+    const [fecha, setFecha] = useState('')
+
+
+    const añadirPartida = (e) => {
+        e.preventDefault()
+
+        //añadir la partida al array con setData
+        const nuevaPartida = {id: data.length +1, avatar: avatar, nick: nick, puntos: puntuacion, fecha: fecha}
+        setData([...data], nuevaPartida)
+    }
+
+    return (
     <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div className="modal-dialog">
             <div className="modal-content">
@@ -8,28 +25,28 @@ export default function VentanaModal() {
                     <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div className="modal-body">
-                    <form >
+                    <form>
                         <div className="mb-3">
                             <label htmlFor="avatar" className="col-form-label">Avatar:</label>
-                            <input type="text" className="form-control" id="avatar"/>
+                            <input type="text" className="form-control" id="avatar" onChange={(e) => setAvatar(e.target.value)} />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="nick" className="col-form-label">Nick:</label>
-                            <input type="text" className="form-control" id="nick"/>
+                            <input type="text" className="form-control" id="nick" onChange={(e) => setNick(e.target.value)}/>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="puntos" className="col-form-label">Puntuación:</label>
-                            <input type="number" className="form-control" id="puntos"/>
+                            <input type="number" className="form-control" id="puntos" onChange={(e) => setPuntuacion(e.target.value)}/>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="fecha" className="col-form-label">Fecha:</label>
-                            <input type="date" className="form-control" id="fecha"/>
+                            <input type="date" className="form-control" id="fecha" onChange={(e) => setFecha(e.target.value)}/>
                         </div>
                     </form>
                 </div>
                 <div className="modal-footer">
                     <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" className="btn btn-primary">Save changes</button>
+                    <button type="button" className="btn btn-primary" onClick={añadirPartida}>Save changes</button>
                 </div>
             </div>
         </div>
