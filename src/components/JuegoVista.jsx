3d5769f3bed77b70c4701bab49e3810f945c1a88
espinozagabriel0 <bascopeespinozagabriel@fpllefia.com>
2025-02-16 13:48:52 +0100

    movimiento piezas y actualizacion de arraycasillas corregido

--- src/components/JuegoVista.jsx
+++ src/components/JuegoVista.jsx
@@ -6,74 +6,72 @@ import { nuevaPieza } from "../lib/nuevaPieza"
 import { modeloPieza } from "../lib/modeloPiezaClass"
 
 export default function JuegoVista() {
-
-  //estado de los modelos 
   const [arrayCasillas, setArrayCasillas] = useState(modelos)
-
-  //estado inicial pieza actual
+  
   const piezaInicial = nuevaPieza(0, Math.floor(Math.random() * 10) + 1)
   const [piezaActual, setPiezaActual] = useState(piezaInicial)
 
-  // comprobar si la pieza actual no sobrepasa el panel 
+
+  // Función para comprobar si una pieza puede colocarse en una columna
   const canSetPieza = (col, lengthPieza) => {
     return col + lengthPieza <= 10
   }
-  
+
+  // Función para crear y colocar una nueva pieza en el tablero
   const insertarNuevaPieza = () => {    
     let colRandom = Math.floor(Math.random() * 10) + 1
 
     if (!canSetPieza(colRandom, piezaActual.matriz[0].length)) {
-        colRandom = 11 - piezaActual.matriz[0].length 
+      colRandom = 11 - piezaActual.matriz[0].length 
     }
     
     setPiezaActual(nuevaPieza(0, colRandom))
   }
 
- 
-
   const pintarPieza = () => {
-    // se copia todo el array, copia completa 
-    const copiaCasillas = arrayCasillas.matriz.map(fila => [...fila]); 
-    // const copiaCasillas = [...arrayCasillas.matriz]; 
-  
-    piezaActual.matriz.forEach((fila, rowIndex) => {
-      fila.forEach((col, colIndex) => {
-        if (col !== 0) {
-          copiaCasillas[piezaActual.fila + rowIndex][piezaActual.columna + colIndex] = col;
-        }
-      });
-    });
-  
-    setArrayCasillas({matriz: copiaCasillas });
-  };
-  
-
-  const borrarPieza = (filaPieza, colPieza) => {
-    const copiaCasillas = arrayCasillas.matriz.map(fila => [...fila]); 
-    // const copiaCasillas = [...arrayCasillas.matriz]; 
+    const copiaCasillas = arrayCasillas.matriz
     
     piezaActual.matriz.forEach((fila, rowIndex) => {
-      fila.forEach((col, colIndex) => {
-        if (col !== 0) {
-            copiaCasillas[filaPieza + rowIndex][colPieza + colIndex] = 0;
-        }
-      });
-    });
-  
-    setArrayCasillas({matriz: copiaCasillas });
-  };
-  
+      if (piezaActual.fila + rowIndex < 21) { 
+        fila.forEach((col, colIndex) => {
+
+          // limites panel izq, der
+          if (col !== 0 && piezaActual.columna + colIndex > 0 && piezaActual.columna + colIndex < 11) { 
+            copiaCasillas[piezaActual.fila + rowIndex][piezaActual.columna + colIndex] = col
+          }
+        })
+      }
+    })
+
+    setArrayCasillas({ matriz: copiaCasillas })
+  }
+
+  const borrarPieza = (filaPieza, colPieza, matriz) => {
+    const copiaCasillas = arrayCasillas.matriz
+    
+    matriz.forEach((fila, rowIndex) => {
+      if (filaPieza + rowIndex < 21) { 
+        fila.forEach((col, colIndex) => {
+          if (col !== 0 && colPieza + colIndex > 0 && colPieza + colIndex < 11) {
+            copiaCasillas[filaPieza + rowIndex][colPieza + colIndex] = 0
+          }
+        })
+      }
+    })
+
+    setArrayCasillas({ matriz: copiaCasillas })
+  }
 
   const girarPieza = () => {
-    
-    
     setPiezaActual(prevPieza => {
-      borrarPieza(prevPieza.fila, prevPieza.columna);
-      
-      let nuevoAngulo = prevPieza.angulo 
-      
+
+      // se borra la pieza actual, para no crear estelas
+      borrarPieza(prevPieza.fila, prevPieza.columna, prevPieza.matriz)
+
+      let nuevoAngulo = prevPieza.angulo;
+
       if (nuevoAngulo < 3) {
-        nuevoAngulo += 1
+        nuevoAngulo++
       }else{
         nuevoAngulo = 0
       }
@@ -81,47 +79,42 @@ export default function JuegoVista() {
       return { 
         ...prevPieza,
         angulo: nuevoAngulo,
-        matriz: prevPieza.matrices[nuevoAngulo], 
+        matriz: prevPieza.matrices[nuevoAngulo]
       }
     })
   }
-  
-  
 
   const bajar = () => {
-
-    borrarPieza(piezaActual.fila, piezaActual.columna)
-
     setPiezaActual(prevPieza => {
-
+      
       if (prevPieza.fila + prevPieza.matriz.length < 21) {
+        borrarPieza(prevPieza.fila, prevPieza.columna, prevPieza.matriz)
+        
         return {...prevPieza, fila: prevPieza.fila + 1}
       }
-
       return prevPieza
     })
   }
 
   const moverIzq = () => {
-    borrarPieza(piezaActual.fila, piezaActual.columna)
-
     setPiezaActual(prevPieza => {
       
-      // borrarPieza(prevPieza.fila, prevPieza.columna)
+      if (prevPieza.columna > 1 && prevPieza.fila + prevPieza.matriz.length < 21) {
+        borrarPieza(prevPieza.fila, prevPieza.columna, prevPieza.matriz)
 
-      if (prevPieza.columna > 1 && prevPieza.fila + prevPieza.matriz.length < 21) { 
         return {...prevPieza, columna: prevPieza.columna - 1}
       }
       return prevPieza
     })
   }
 
-  const moverDra = () => {
-    borrarPieza(piezaActual.fila, piezaActual.columna)
 
+  const moverDra = () => {
     setPiezaActual(prevPieza => {
-      // borrarPieza(prevPieza.fila, prevPieza.columna)
+      
       if (prevPieza.columna + prevPieza.matriz[0].length <= 10 && prevPieza.fila + prevPieza.matriz.length < 21) {
+        borrarPieza(prevPieza.fila, prevPieza.columna, prevPieza.matriz)
+
         return {...prevPieza, columna: prevPieza.columna + 1}
       }
       return prevPieza
@@ -129,34 +122,31 @@ export default function JuegoVista() {
   }
 
   const controlTeclas = (event) => {
-
     switch (event.key) {
-        case 'ArrowUp':
-          girarPieza()
+      case 'ArrowUp':
+        girarPieza()
         break;
       case 'ArrowDown':
-          bajar()
+        bajar()
         break;
       case 'ArrowLeft':
-          moverIzq()
+        moverIzq()
         break;
       case 'ArrowRight':
-          moverDra()
+        moverDra()
         break;
       default:
         break;
     }
   }
 
+
   useEffect(() => {
-    window.addEventListener('keydown', controlTeclas);
-
+    window.addEventListener('keydown', controlTeclas)
     return () => {
-      window.removeEventListener('keydown', controlTeclas); 
-    };
-
+      window.removeEventListener('keydown', controlTeclas)
+    }
   }, [])
-
 
   useEffect(() => {
     pintarPieza()
@@ -164,60 +154,60 @@ export default function JuegoVista() {
     if (piezaActual.fila + piezaActual.matriz.length < 21) {
       const intervalID = setInterval(() => {
         bajar()
-      }, 1000);
+      }, 1000)
       
-      return () => {  
+      return () => {
         clearInterval(intervalID)
       }
-    }else{
+    } else {
       insertarNuevaPieza()
     }
-
   }, [piezaActual])
 
   return (
     <section className="vista-juego p-2">
-        <div className="d-flex gap-3 text-white mx-auto p-2" style={{maxWidth: "68rem", fontSize: "1.5rem"}}>
-            <section className="d-flex flex-column gap-2">
-                <div className="rounded p-4 text-center border">Guardado</div>
-                <div className="rounded p-4 border d-flex flex-column align-items-center justify-content-center">
-                    <p>Tiempo</p>
-                    <span>0</span>
-                </div>
-                <div className="rounded p-4 d-flex flex-column gap-2 border">
-                    <div className="d-flex flex-column align-items-center justify-content-center">
-                        <p>Nivel</p>
-                        <span>1</span>
-                    </div>
-                    <hr />
-                    <div className="d-flex flex-column align-items-center justify-content-center">
-                        <p>Puntuación</p>
-                        <span>0</span>
-                    </div>
-                    <hr />
-                    <div className="d-flex flex-column align-items-center justify-content-center">
-                        <p>Lineas</p>
-                        <span>0</span>
-                    </div>
-                </div>
-            </section>
-
-            <div id="juego-container" className=" rounded bg-dark bg-opacity-50">
-              <Panel modelos={arrayCasillas.matriz}/>
+      <div className="d-flex gap-3 text-white mx-auto p-2" style={{maxWidth: "68rem", fontSize: "1.5rem"}}>
+        <section className="d-flex flex-column gap-2">
+          <div className="rounded p-4 text-center border">Guardado</div>
+          <div className="rounded p-4 border d-flex flex-column align-items-center justify-content-center">
+            <p>Tiempo</p>
+            <span>0</span>
+          </div>
+          <div className="rounded p-4 d-flex flex-column gap-2 border">
+            <div className="d-flex flex-column align-items-center justify-content-center">
+              <p>Nivel</p>
+              <span>1</span>
             </div>
-            
-            <section className="d-flex flex-column gap-2 p-5">
-                <div className="border rounded p-4">
-                    <p>Siguiente</p>
-                    {/*  */}
-                </div>
-                <div className="border rounded p-2 d-flex flex-column gap-2">
-                    <button className="btn btn-success" onClick={() => iniciarMovimieno()}>JUGAR</button>
-                    <button className="btn btn-info">PAUSA</button>
-                    <button className="mt-3 btn btn-warning" onClick={() => insertarNuevaPieza()}>Insertar pieza</button>
-                </div>
-            </section>
+            <hr />
+            <div className="d-flex flex-column align-items-center justify-content-center">
+              <p>Puntuación</p>
+              <span>0</span>
+            </div>
+            <hr />
+            <div className="d-flex flex-column align-items-center justify-content-center">
+              <p>Lineas</p>
+              <span>0</span>
+            </div>
+          </div>
+        </section>
+
+        <div id="juego-container" className="rounded bg-dark bg-opacity-50">
+          <Panel modelos={arrayCasillas.matriz}/>
         </div>
+        
+        <section className="d-flex flex-column gap-2 p-5">
+          <div className="border rounded p-4">
+            <p>Siguiente</p>
+          </div>
+          <div className="border rounded p-2 d-flex flex-column gap-2">
+            <button className="btn btn-success">JUGAR</button>
+            <button className="btn btn-info">PAUSA</button>
+            <button className="mt-3 btn btn-warning" onClick={() => insertarNuevaPieza()}>
+              Insertar pieza
+            </button>
+          </div>
+        </section>
+      </div>
     </section>
   )
 }
